de95158650f7e1cab416119cab33aed5a3919286
Denys Viliuzhanin <dvilyuzhanin@gmail,com>
2015-02-01 20:11:50 +0200

    Item description input

--- dropbucket/public/js/component/DropItPanelComponent.js
+++ dropbucket/public/js/component/DropItPanelComponent.js
@@ -1,8 +1,11 @@
 
 define(['text!dropIt.panel.html'], function(template) {
 	var DROPPED_ITEM_EVENT = 'droppedItemEvent';
+	var DROP_IT_PANEL_SELECTOR = '.dropItPanel';
+	var DESCRIPTION_INPUT_SELECTOR = '.descriptionInput'; 
+	var DROP_IT_BUTTON_SELECTOR = '.dropItButton';
+	
 	return Backbone.View.extend({
-		
 		
 		initialize: function() {
 			
@@ -11,10 +14,25 @@ define(['text!dropIt.panel.html'], function(template) {
 			this.$el = $(template);
 			
 			
-			this.$el.find('.dropItButton').click(_.bind(function(){
-				this.trigger(DROPPED_ITEM_EVENT, "new item");
-			}, this));
+			this.$el.find(DESCRIPTION_INPUT_SELECTOR)
+				.keypress(_.bind(this.handleKeyPressedOnDescriptionInput, this));
+			this.$el.find(DROP_IT_BUTTON_SELECTOR)
+				.click(_.bind(this.fireDroppedItemEvent, this));
+			
 			return this;
+		},
+		
+		handleKeyPressedOnDescriptionInput: function(e) {
+			if(e.which == 13) {
+				this.fireDroppedItemEvent();
+			}
+		},
+		
+		fireDroppedItemEvent: function() {
+			var $input = this.$el.find(DESCRIPTION_INPUT_SELECTOR);
+			var value = $input.val();
+			this.trigger(DROPPED_ITEM_EVENT, value);
+			$input.val('');
 		},
 		
 		onDroppedItem: function(listener) {
