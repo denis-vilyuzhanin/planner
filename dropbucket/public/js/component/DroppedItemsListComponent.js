ee3902ce37fe69b3ffbd24b93309f1a0fea7a243
Denys Viliuzhanin <dvilyuzhanin@gmail,com>
2015-02-02 09:54:56 +0200

    Adding item from template

--- dropbucket/public/js/component/DroppedItemsListComponent.js
+++ dropbucket/public/js/component/DroppedItemsListComponent.js
@@ -1,15 +1,16 @@
 define(['text!DroppedItemsList.component.html'], function(template){
 	var $listTemplate = $(template);
-	var $itemTemplate = $listTemplate.remove('itemTemplate');
-	$itemTemplate.remove();
+	var $itemTemplate = $listTemplate.find('.dayItem').remove();
 	
 	return Backbone.View.extend({
 	
 		
 		
 		render: function() {
-			this.$el = $listTemplate;
-			
+			this.$el = $listTemplate.clone();
+			var $newItem = $itemTemplate.clone();
+			$newItem.find('.description').text("Item");
+			this.$el.append($newItem)
 			return this;
 		}
 	});
